af07a46865c3fe9261e854c0079f792db47b8ceb
nozomi-nakamitsu <nozomi_nakamitsu@arsaga.jp>
2021-05-05 09:51:37 +0900

    swiperで画像スライドショー実装

--- src/component/MV.tsx
+++ src/component/MV.tsx
@@ -9,6 +9,20 @@ import ArrowBackIosTwoToneIcon from '@material-ui/icons/ArrowBackIosTwoTone';
 import ArrowForwardIosTwoToneIcon from '@material-ui/icons/ArrowForwardIosTwoTone';
 import OndemandVideoRoundedIcon from '@material-ui/icons/OndemandVideoRounded';
 import VideoModal from "./VideoModal"
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/swiper.min.css";
+import "swiper/components/pagination/pagination.min.css"
+import "swiper/components/navigation/navigation.min.css"
+// import Swiper core and required modules
+import SwiperCore, {
+  Keyboard,Pagination,Navigation
+} from 'swiper/core';
+import { pink } from '@material-ui/core/colors';
+
+// install Swiper modules
+SwiperCore.use([Keyboard,Pagination,Navigation]);
 type Props = {
 
 }
@@ -19,40 +33,40 @@ const MV = () => {
   const refDom = useRef<any | null>(null);
   const [videoopen, setvideoOpen] = useState(false);
   // 画像のスライドショー
-  const ChangeImage=(num :number)=>{
-    var MVimage =  document.getElementsByClassName("MVimage")
-    MVimage[0].setAttribute("src", image_src[num]);
-    // まるボタンの色切り替え
-    if (!refDom.current) {
-    var button =  document.getElementsByClassName(`button${num}`)
-    refDom.current = button[0]
-    }else{
-      refDom.current.classList.remove("-active");
-      var button =  document.getElementsByClassName(`button${num}`)
-      button[0].classList.add("-active")
-      refDom.current = button[0]
-    }
-  }
-  const goBack=()=>{
-    if (num == 0) {
-      setnum(2) ;
-    }else {
-       setnum(num -1);
-    }
-    ChangeImage(num);
-  }
-  const goForward=()=>{
-    if (num == 2) {
-      setnum(0) ;
-    }else {
-       setnum(num +1);
-    }
-    ChangeImage(num);
-  }
-  const SwitchImage=(i :number)=>{
-  setnum(i)
-    ChangeImage(num);
-  }
+  // const ChangeImage=(num :number)=>{
+  //   var MVimage =  document.getElementsByClassName("MVimage")
+  //   MVimage[0].setAttribute("src", image_src[num]);
+  //   // まるボタンの色切り替え
+  //   if (!refDom.current) {
+  //   var button =  document.getElementsByClassName(`button${num}`)
+  //   refDom.current = button[0]
+  //   }else{
+  //     refDom.current.classList.remove("-active");
+  //     var button =  document.getElementsByClassName(`button${num}`)
+  //     button[0].classList.add("-active")
+  //     refDom.current = button[0]
+  //   }
+  // }
+  // const goBack=()=>{
+  //   if (num == 0) {
+  //     setnum(2) ;
+  //   }else {
+  //      setnum(num -1);
+  //   }
+  //   ChangeImage(num);
+  // }
+  // const goForward=()=>{
+  //   if (num == 2) {
+  //     setnum(0) ;
+  //   }else {
+  //      setnum(num +1);
+  //   }
+  //   ChangeImage(num);
+  // }
+  // const SwitchImage=(i :number)=>{
+  // setnum(i)
+  //   ChangeImage(num);
+  // }
   
 
 
@@ -72,24 +86,19 @@ const MV = () => {
 <div className="MV">
 <div className="MVCopntainer">
 <div id="main">
-  <div className="ImageArea">
-  <img src={makeyouhappy} alt=""  className="MVimage"/>
-  <OndemandVideoRoundedIcon className="ondemandVideoRoundedIcon" style={{ fontSize: 40 , opacity:0.7}} onClick={()=>openvideoModal()}/>
-  </div>
-
-<div className="ButtonArea">
-  <ArrowBackIosTwoToneIcon style={{ fontSize: 20 ,color:"#DDDDDD"}} className="leftArrow" onClick={()=>goBack()}/>
-  <div className="centerButton">
-  <FiberManualRecordSharpIcon style={{ fontSize: 20 }} className={`button${0}  -active` } onClick={()=>SwitchImage(0)}/>
-  <FiberManualRecordSharpIcon style={{ fontSize: 20 }} className={`button${1}`} onClick={()=>SwitchImage(1)}/>
-  <FiberManualRecordSharpIcon style={{ fontSize: 20 }} className={`button${2}`} onClick={()=>SwitchImage(2)}/>
-  </div>
- <ArrowForwardIosTwoToneIcon style={{ fontSize: 20 ,color:" #DDDDDD"}} className="rightArrow" onClick={()=>goForward()}/>
-</div>
+<OndemandVideoRoundedIcon className="ondemandVideoRoundedIcon" style={{ fontSize: 40 , opacity:0.7}} onClick={()=>openvideoModal()}/>
+<Swiper slidesPerView={1} spaceBetween={30} keyboard={{
+  "enabled": true
+}} pagination={{
+  "clickable": true
+}} navigation={true} className="mySwiper">
+  <SwiperSlide> <img src={makeyouhappy} alt=""/></SwiperSlide><SwiperSlide><img src={takeapicture} alt=""/></SwiperSlide><SwiperSlide><img src={poppinshakin} alt=""/></SwiperSlide>
+  </Swiper>
 
 </div>
 </div>
 </div>
+
 </>
 
 
